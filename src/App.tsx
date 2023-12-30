9e191a823848040a65bdcc04780409a3dde4eecd
Attasiem Kingsley <attasiemj@gmail.com>
2023-12-30 02:22:02 +0100

    fix: add app theme fonts utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { ThemeProvider } from "./theme";
+import { Box } from "./components/base/box";
+import { Text } from "./components/base/text";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ThemeProvider>
+    <Box style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    </Box>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/config/palette.ts
@@ -0,0 +1,15 @@
+export const palette = {
+  black: "#000000",
+  black40: "rgba(0, 0, 0, 0.4)",
+  error: "#FF0101",
+  success: "#76EE59",
+  transparent: "transparent",
+  white: "#FFFFFF",
+  white40: "rgba(255, 255, 255, 0.4)",
+  purpleLight: "#8C6FF7",
+  purplePrimary: "#5A31F4",
+  purpleDark: "#3F22AB",
+  greenLight: "#56DCBA",
+  greenPrimary: "#0ECD9D",
+  greenDark: "#0A906E",
+};
